d06d45855b0a7fe62b73a33ba38093356fb4afcc
Mike Chen <517926804@qq.com>
2021-02-20 16:20:03 +0800

    update rule

--- config.def.h
+++ config.def.h
@@ -40,9 +40,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance    title   tags mask  switchtag  isfloating   monitor */
-	{ "Gimp",           NULL,       NULL,   0,         1,         1,           -1 },
-	{ "Firefox",        NULL,       NULL,   1 << 8,    1,         0,           -1 },
+	/* class              instance    title   tags mask  switchtag  isfloating   monitor */
+	{ "Emacs",            NULL,       NULL,       1,         1,         0,           -1 },
+	{ "Firefox",          NULL,       NULL,       1 << 1,    1,         0,           -1 },
+	{ "Google-chrome",    NULL,       NULL,       1 << 1,    1,         0,           -1 },
+	{ "TelegramDesktop",  NULL,       NULL,       1 << 2,    3,         1,           -1 },
 };
 
 /* layout(s) */
