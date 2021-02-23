abd6ebf1fcd8894dc349f3d41512f164a4ba74f0
Mike Chen <517926804@qq.com>
2021-02-23 12:06:03 +0800

    update dmenucmd

--- config.def.h
+++ config.def.h
@@ -74,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bgu, "-nf", col_fgu, "-sb", col_bdf, "-sf", col_bgu, "-p", "Run:", "-c", "-g", "4", "-l", "10", "-bw", "1", "-F", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bgu, "-nf", col_fgu, "-sb", col_bdf, "-sf", col_bgu, "-p", "Run:", "-c", "-g", "4", "-l", "10", "-bw", "1", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, NULL };
